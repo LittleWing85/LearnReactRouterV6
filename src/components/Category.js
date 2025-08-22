323fbf051df59a78968d62e83ffbf6f787f00e57
littlewing <mail@alexandra-jaeger.net>
2025-08-22 21:14:15 +0200

    nested routing added

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectCategories } from '../features/categories/categoriesSlice';
-import { selectArticles } from '../features/articles/articlesSlice';
-import { Link, useParams } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectCategories } from "../features/categories/categoriesSlice";
+import { selectArticles } from "../features/articles/articlesSlice";
+import { Link, useParams } from "react-router-dom";
 
-export default function Category () {
-  const categories = useSelector(selectCategories)
-  const articles = useSelector(selectArticles)
+export default function Category() {
+    const categories = useSelector(selectCategories);
+    const articles = useSelector(selectArticles);
 
-  const { name } = useParams()
+    const { categoryname } = useParams();
 
-  return (
-    <div>
-      <h2>Articles Related to {name}</h2>
-      <ul>
-        { categories[name].map(articleSlug => (
-          <li>
-            <Link to={`/articles/${articleSlug}`}>{articles[articleSlug].title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+    return (
+        <div>
+            <h2>Articles Related to {categoryname}</h2>
+            <ul>
+                {categories[categoryname].map((articleSlug) => (
+                    <li>
+                        <Link to={`/articles/${articleSlug}`}>
+                            {articles[articleSlug].title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
